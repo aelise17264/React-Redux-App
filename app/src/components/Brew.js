f2ae3c979fea7275eb93f986e3ed6799bbeca98d
unknown <aelise17264@gmail.com>
2020-09-09 15:55:31 -0600

    showing all relevant info on page

--- app/src/components/Brew.js
+++ app/src/components/Brew.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 function Brew({drink}){
-return <p>{drink.name}</p>
+return (
+<div claassName='card'>
+<h3>{drink.name}</h3>
+<p>Address: {drink.street} {drink.city}, {drink.state} {drink.postal_code}</p>
+<p>Type of Brew: {drink.brewery_type}</p>
+<p>Call: {drink.phone}</p>
+<p>Website: {drink.website_url}</p>
+<p>Geocache: {drink.longitude}, {drink.latitude}</p>
+</div>
+)
+
 }
 export default Brew;
