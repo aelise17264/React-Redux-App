153bffc82717f3dca0fdda85c8a07f0c66cd18fe
unknown <aelise17264@gmail.com>
2020-09-09 16:43:42 -0600

    final styles

--- app/src/components/Brew.js
+++ app/src/components/Brew.js
@@ -4,7 +4,7 @@ function Brew({drink}){
 return (
 <div className='cardMaker'>
 <h2><span>🍺</span>{drink.name}</h2>
-<p>Address: {drink.street} {drink.city},</p>
+<p>Address: {drink.street}, {drink.city}</p>
 <p> {drink.state} {drink.postal_code}</p>
 <p>Type of Brew: {drink.brewery_type}</p>
 <p>Call: {drink.phone}</p>
