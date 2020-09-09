1ce54eb14a0dbb88273dc54167a169678151408f
unknown <aelise17264@gmail.com>
2020-09-09 14:37:16 -0600

    index & basic reducer set up

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-dom';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleWare} from 'redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from './reducer'
+
+const store = createStore(reducer, applyMiddleWare(thunk, logger))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
